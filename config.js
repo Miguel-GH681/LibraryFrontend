7dc17ea4e134ee7b426b985d40134adf5f94bc4e
Miguel-GH681 <101464772+Miguel-GH681@users.noreply.github.com>
2022-05-31 17:28:18 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports = {
         username: "S4ngr34m4r1ll4",
         password: "Pr0gr4m4r3sd1v3rt1d0",
         database: "biblioteca_digital",
-        host: "sqlserverlibraryapp.database.windows.net"
+        host: "sqlserverlibrary.database.windows.net"
     }
 }
