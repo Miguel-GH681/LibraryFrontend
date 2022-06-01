065c62fabbeba4efe327ff52f00d3f477070fe57
Miguel-GH681 <miguelgh681@gmail.com>
2022-05-31 22:25:55 -0500

    adición de modal de usuario

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -4,15 +4,18 @@ const router = express.Router();
 const Book = require('../models/bookModel');
 const Genero = require('../models/genreModel');
 
-router.route('/getBooks').get((req, res)=>{
+router.route('/getBooks/:orden').get((req, res)=>{
     Book.findAll({
         include: {
             model: Genero,
             attributes: ['nombre']
-        }
+        },
+        order:[
+            ['titulo', req.params.orden]
+        ]
     }).then(result=>{
         res.json(result);
     })
-});
+}); 
 
 module.exports = router;
